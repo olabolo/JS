962ba0afc1a22006e0680b6cd5f8c4c8c90b9339
olabolo <baigarovy@gmail.com>
2021-07-12 23:09:41 +0300

    12/07

---
+++ lesson_2/task_3.js
@@ -0,0 +1,12 @@
+let a = 8;
+let b = -7;
+
+if (a >= 0, b >= 0) {
+    console.log('Разность чисел  a и b ', a - b);
+}
+else if (a < 0, b < 0) {
+    console.log('Произведение чисел  a и b ', a * b);
+}
+else {
+    console.log('Сумма чисел  a и b ', a + b);
+}
