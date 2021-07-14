3e3bc5cd7a3d3f2e5981bac071dde888a5349249
olabolo <baigarovy@gmail.com>
2021-07-14 16:40:44 +0300

    дз исправление переменных в 5 задании

--- lesson_2/task_5.js
+++ lesson_2/task_5.js
@@ -1,5 +1,5 @@
-let arg1 = +prompt('Введите число a');
-let arg2 = +prompt('Введите число b');
+let arg1 = +prompt('Введите число arg1');
+let arg2 = +prompt('Введите число arg2');
 
 function summa(x, y) {
     return (x + y);
@@ -13,6 +13,6 @@ function del(x, y) {
 function umn(x, y) {
     return (x * y);
 }
-let result = 'Сумма чисел a и b: ' + summa(a, b) + ', Вычитание чисел a и b: ' + minus(a, b) + ' Умножение чисел a и b: ' + umn(a, b) + ' Деление чисел a и b: ' + del(a, b);
-console.log(a, b, result);
+let result = 'Сумма чисел arg1 и arg2: ' + summa(arg1, arg2) + ', Вычитание чисел arg1 и arg2: ' + minus(arg1, arg2) + ' Умножение чисел arg1 и arg2: ' + umn(arg1, arg2) + ' Деление чисел arg1 и arg2: ' + del(arg1, arg2);
+console.log(arg1, arg2, result);
 alert(result);
